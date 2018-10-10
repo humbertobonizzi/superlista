7541ed6f2ae2afcabf096956dabf8afae30a1e9b
Humberto Bonizzi <humbertobonizzi@MacBook-Pro-de-Humberto.local>
2018-10-09 22:20:58 -0300

    Primeiro Teste Completo

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,8 @@ from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.shortcuts import render
+from django.http import HttpRequest
+
 # Create your tests here.
 class HomePageTest(TestCase):
 
@@ -9,6 +11,14 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+        
 
 
 
