ade91875cd7e41f5a5c4a8a75c53ce9b6f6f7205
Humberto Bonizzi <humbertobonizzi@MacBook-Pro-de-Humberto.local>
2018-10-09 21:23:51 -0300

    commite teste completoi

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Safari()
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Safari()
 
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
 
-assert 'Django' in browser.title
+def main():
+    unittest.main(warnings='ignore')
+    pass
+
+if __name__ == '__main__':
+    main()
