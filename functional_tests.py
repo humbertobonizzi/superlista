cf618a959131841f308b8d3c21d6ebfd8d75d4cf
Humberto Bonizzi <humbertobonizzi@MacBook-Pro-de-Humberto.local>
2018-10-10 21:47:30 -0300

    Teste refatorado

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,8 +13,26 @@ class NewVisitorTest(unittest.TestCase):
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
-        #self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do '
+        )
+        inputbox.send_keys('Buy a peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacok feathers' for row in rows )
+        )
+
+        self.fail('Finish the test')
 
 def main():
     unittest.main(warnings='ignore')
